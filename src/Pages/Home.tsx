813fd3ec5a8964a8425515a0bbb2f12efbf1e1df
dustytheaverage <60454332+dustytheaverage@users.noreply.github.com>
2024-10-11 17:11:32 -0500

    Animated subheader

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import '../App.css'
+import TypewriterComponent from 'typewriter-effect'
 
 function Home() {
+  const amA = ['Computer Science Graduate.', 'Software Engineer.', 'Game Developer.', 'Web Developer.', 'Freelancer.'];
   return (
     <div className = 'home'>
       <h1 className='header'>Dustin Anderson</h1>
+      <h2 className='subheader'>
+        <TypewriterComponent options={{
+          strings: amA,
+          autoStart: true,
+          loop: true
+        }}/>
+      </h2>
     </div>
   )
 }
