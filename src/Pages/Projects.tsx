3dd074d40b4b349d949b199f974e7771ebe5dc3d
dustytheaverage <60454332+dustytheaverage@users.noreply.github.com>
2024-10-28 12:13:05 -0500

    Fleshed out Project Item assuming the existence of an image.

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -29,9 +29,9 @@ function Projects() {
         <h1 className='header'>Projects</h1>
         <p className='paragraph'>The following are a collection of projects I have completed, am working on, or am planning on working on that I am proud enough to share with you.
           Some were solo projects, others I was fortunate enough to work alongside individuals with great talents of their own. All of them have led to anecdotes I hold dearly in my
-          mind, of plunging towards the depths of insanity before reaching the breakthrough I was looking for, often something mundane yet triumphant all the same. All of them culminate
-          into the reason I continue to develop despite the hardships: that implicit joy of creation for creations sake, regardless of what audience might await. <br/><br/>That being said, I hope
-          you find some of these projects interesting, or amusing at the very least, if not always enjoyable.
+          mind, of plunging towards the depths of insanity before reaching the breakthrough I was looking for, often something mundane yet triumphant all the same. 
+          All of them culminate into the reason I continue to develop despite the hardships: that implicit joy of creation for creations sake, regardless of what audience might await. 
+          <br/><br/>That being said, I hope you find some of these projects interesting, or amusing at the very least, if not always enjoyable.
         </p>
       </div>
       
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -18,6 +18,16 @@ interface projectData {
   state: State
 }
 
+function ProjectLinks({texts, links}: {texts: string[], links: string[]}){
+  var output = '';
+
+  for(let i = 0; i < texts.length; i++){
+    output += <a href={links[i]}>{texts[i]}  </a>;
+  }
+
+  return(output)
+}
+
 function Project({project, classVar}: {project: projectData, classVar: string}) {
 
   var hasImage = false;
@@ -26,7 +36,28 @@ function Project({project, classVar}: {project: projectData, classVar: string}) 
   }
 
   return (
-    <div className={classVar}>Project</div>
+    <div className={classVar}>
+      <table className='projectTable'>
+        <tr>
+          <th className={'col1'} style={{fontSize: 48}}>{project.title} - {project.type}</th>
+          <th className='col2'></th>
+        </tr>
+        <tr>
+          <td style={{fontSize: 32}}>Position - {project.position}</td>
+        </tr>
+        <tr>
+          <td style={{fontSize: 24}}>Description: {project.description}</td>
+          <td><img src={project.image} /></td>
+        </tr>
+        <tr>
+          <td style={{fontSize: 32}}>Primary Skills: {project.skills}</td>
+        </tr>
+        <tr>
+          <td style={{fontSize: 32}}>Links: <ProjectLinks texts={project.linkNames} links={project.links} /></td>
+        </tr>
+
+      </table>
+    </div>
   )
 }
 
