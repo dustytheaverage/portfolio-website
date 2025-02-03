cb94cfe3b968d58797e122787e7335bdff4a0f82
dustytheaverage <60454332+dustytheaverage@users.noreply.github.com>
2025-02-03 12:18:06 -0600

    Project Generation

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,29 +1,16 @@
 import React from 'react'
 import projectData from '../components/projectData';
+import Project from '../components/Project';
+import ProjectList from '../projects.json';
 import '../App.css'
 
-function Projects() {
-  
+function Projects(this: any) {
   //Variables for setting the next project's css class
   let projClass = 1;
   const projClass1 = 'projectType1';
   const projClass2 = 'projectType2';
   let currentProjClass = '';
 
-  switch (projClass){//decide which css class will be used for the next project
-    case 1:
-      currentProjClass = projClass1;
-      projClass = 2;
-      break;
-    case 2:
-      currentProjClass = projClass2;
-      projClass = 1;
-      break;
-    default:
-      projClass = 1;
-      break;
-  }
-  
   return (
     <div>
       <div className = 'projectsPreface'>
@@ -32,10 +19,40 @@ function Projects() {
           Some were solo projects, others I was fortunate enough to work alongside individuals with great talents of their own. All of them have led to anecdotes I hold dearly in my
           mind, of plunging towards the depths of insanity before reaching the breakthrough I was looking for, often something mundane yet triumphant all the same. 
           All of them culminate into the reason I continue to develop despite the hardships: that implicit joy of creation for creations sake, regardless of what audience might await. 
-          <br/><br/>That being said, I hope you find some of these projects interesting, or amusing at the very least, if not always enjoyable.
+          <br/><br/>That being said, I hope you find some of these projects interesting, amusing, or maybe even enjoyable.
         </p>
       </div>
-      
+      <div >
+        {ProjectList.map(project => {
+          let projData: projectData = 
+          {title: project.title,
+            type: project.type,
+            position: project.position,
+            description: project.description,
+            skills: project.skills,
+            linkNames: project.linkNames as string[],
+            links: project.links as string[],
+            image: project.image,
+            state: project.state
+          };
+          switch (projClass){//decide which css class will be used for the next project
+            case 1:
+              currentProjClass = projClass1;
+              projClass = 2;
+              break;
+            case 2:
+              currentProjClass = projClass2;
+              projClass = 1;
+              break;
+            default:
+              projClass = 1;
+              break;
+          }
+          
+          
+          return(Project(projData,currentProjClass))
+        })}
+      </div>
       
     </div>
   )
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -19,37 +19,65 @@ function ProjectLinks({texts, links}: {texts: string[], links: string[]}){
   return(output)
 }
 
-function Project({project, classVar}: {project: projectData, classVar: string}) {
+function Project(project: projectData, classVar: string) {
 
   var hasImage = false;
   if(project.image != null){
     hasImage = true;
   }
 
-  return (
-    <div className={classVar}>
-      <table className='projectTable'>
-        <tr>
-          <th className={'col1'} style={{fontSize: 48}}>{project.title} - {project.type}</th>
-          <th className='col2'></th>
-        </tr>
-        <tr>
-          <td style={{fontSize: 32}}>Position - {project.position}</td>
-        </tr>
-        <tr>
-          <td style={{fontSize: 24}}>Description: {project.description}</td>
-          <td><img src={project.image} /></td>
-        </tr>
-        <tr>
-          <td style={{fontSize: 32}}>Primary Skills: {project.skills}</td>
-        </tr>
-        <tr>
-          <td style={{fontSize: 32}}>Links: <ProjectLinks texts={project.linkNames} links={project.links} /></td>
-        </tr>
-
-      </table>
-    </div>
-  )
+  if(hasImage === true){
+    return (
+      <div className={classVar}>
+        <table className='projectTable'>
+          <tr>
+            <th className={'col1'} style={{fontSize: 48}}>{project.title} - {project.type}</th>
+            <th className='col2'></th>
+          </tr>
+          <tr>
+            <td style={{fontSize: 32}}>Position - {project.position}</td>
+          </tr>
+          <tr>
+            <td style={{fontSize: 24}}>Description: {project.description}</td>
+            <td><img src={project.image} /></td>
+          </tr>
+          <tr>
+            <td style={{fontSize: 32}}>Primary Skills: {project.skills}</td>
+          </tr>
+          <tr>
+            <td style={{fontSize: 32}}>Links: <ProjectLinks texts={project.linkNames} links={project.links} /></td>
+          </tr>
+  
+        </table>
+      </div>
+    )
+  }
+  else{
+    return (
+      <div className={classVar}>
+        <table className='projectTable'>
+          <tr>
+            <th className={'col1'} style={{fontSize: 48}}>{project.title} - {project.type}</th>
+            <th className='col2'></th>
+          </tr>
+          <tr>
+            <td style={{fontSize: 32}}>Position - {project.position}</td>
+          </tr>
+          <tr>
+            <td style={{fontSize: 24}}>Description: {project.description}</td>
+          </tr>
+          <tr>
+            <td style={{fontSize: 32}}>Primary Skills: {project.skills}</td>
+          </tr>
+          <tr>
+            <td style={{fontSize: 32}}>Links: <ProjectLinks texts={project.linkNames} links={project.links} /></td>
+          </tr>
+  
+        </table>
+      </div>
+    )
+  }
+  
 }
 
 export default Project
